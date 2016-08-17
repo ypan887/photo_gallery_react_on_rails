56c5920b185c34cc9e7df3ca5be0a527fab625a4
ypan887 <ypan887@gmail.com>
2016-08-16 21:42:40 -0700

    submit button was disabled until all input field was provided

--- client/app/bundles/PhotoGallery/components/UploadComponent.jsx
+++ client/app/bundles/PhotoGallery/components/UploadComponent.jsx
@@ -5,6 +5,18 @@ import FontAwesome from 'react-fontawesome';
 export default class UploadComponent extends React.Component {
   constructor() {
     super();
+    this.state = {
+      title: '',
+      desc: '',
+      image: ''
+    }
+  }
+
+  handleChange(e) {
+    name = e.target.name.match(/\[(.*?)\]/)[1];
+    this.setState({
+      [name]: e.target.value
+    });
   }
 
   showModal() {
@@ -20,6 +32,7 @@ export default class UploadComponent extends React.Component {
   }
 
   render() {
+    const validForm = this.state.title && this.state.desc && this.state.image;
     return (
       <div>
         <FontAwesome onClick={ this.showModal.bind(this) } name='plus-circle' size='3x' id='upload-button' />
@@ -30,24 +43,24 @@ export default class UploadComponent extends React.Component {
               <div className="row">
                 <input type='hidden' name='authenticity_token' value={ this.props.csrfToken } />
                 <div className="input-field col s12">
-                  <input placeholder="Title" name="image[title]" type="text" className="validate" />
+                  <input placeholder="Title" name="image[title]" type="text" className="validate" value={ this.state.title } onChange={ this.handleChange.bind(this) } />
                   <label htmlFor="first_name">Title</label>
                 </div>
                 <div className="input-field col s12">
-                  <input name="image[desc]" type="text" className="validate" />
+                  <input name="image[desc]" type="text" className="validate" value={ this.state.desc } onChange={ this.handleChange.bind(this) } />
                   <label htmlFor="desc">Description</label>
                 </div>
                 <div className="file-field input-field col s12">
                   <div className="btn">
                     <span>Choose a image</span>
-                    <input type="file" name="image[image]" accept="image/*" />
+                    <input type="file" name="image[image]" accept="image/*" value={ this.state.image } onChange={ this.handleChange.bind(this) } />
                   </div>
                   <div className="file-path-wrapper">
                     <input className="file-path validate" type="text" />
                   </div>
                 </div>
                 <div className="col s12">
-                  <button type='submit' className='submit btn waves-effect waves-light' name="action">Upload</button>
+                  <button type='submit' className='submit btn waves-effect waves-light' name="action" disabled={ !validForm }>Upload</button>
                 </div>
               </div>
             </form>
